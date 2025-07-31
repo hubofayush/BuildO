73e2dbca9c9eee60e11dbdc603589c4b6b341b96
hubofayush <ayushempire03@gmail.com>
2025-07-31 20:00:53 +0530

    feat: add AITools component and integrate it into Home page; update Hero component with navigation and demo link

--- client/src/Components/Hero.jsx
+++ client/src/Components/Hero.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { assets } from "../assets/assets";
-
+import { useNavigate } from "react-router-dom";
 const Hero = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="px-4 sm:px-20 xl:px-32 relative inline-flex flex-col w-full justify-center bg-[url(/gradientBackground.png)] bg-cover bg-no-repeat min-h-screen">
       <div className="text-center">
@@ -20,11 +22,22 @@ const Hero = () => {
         </p>
       </div>
       <div className="flex flex-wrap justify-center gap-4 text-sm max-sm:text-xs mt-4">
-        <button className="bg-primary rounded-lg cursor-pointer px-10 py-3 text-white hover:scale-102 active:scale-95 transition shadow-lg ">
+        <button
+          className="bg-primary rounded-lg cursor-pointer px-10 py-3 text-white hover:scale-102 active:scale-95 transition shadow-lg "
+          onClick={() => {
+            navigate("/ai");
+          }}
+        >
           Start generating now
         </button>
         <button className="bg-white px-10 py-3 rounded-lg border border-gray-300 hover:scale-102 active:scale-95 transition cursor-pointer shadow-lg">
-          Watch demo
+          <a
+            href="https://youtu.be/RkYIWg5XAnI?si=wI9KgAJk0A6fPp8X"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Watch demo
+          </a>
         </button>
       </div>
       <div className="flex justify-center gap-4 mx-auto mt-8 text-gray-600">
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
 import Hero from "../Components/Hero";
+import AITools from "../Components/AITools";
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <AITools />
     </>
   );
 };
