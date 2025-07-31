17190c350fae7cd2526b0e16e1373f392add16c5
hubofayush <ayushempire03@gmail.com>
2025-07-31 20:38:53 +0530

    feat: add Testimonials component and integrate it into Home page; update AITools component styles and fix typo

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 import Hero from "../Components/Hero";
 import AITools from "../Components/AITools";
+import Testimonial from "../Components/Testimonials";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Navbar />
       <Hero />
       <AITools />
+      <Testimonial />
     </>
   );
 };
