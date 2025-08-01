17d76f764ee745bcb34af46f030d6f71c8708b6e
hubofayush <ayushempire03@gmail.com>
2025-08-01 23:37:18 +0530

    feat: implement Layout and Sidebar components with user authentication and navigation

--- client/src/Pages/Layout.jsx
+++ client/src/Pages/Layout.jsx
@@ -1,11 +1,46 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { assets } from "../assets/assets";
+import { Menu, X } from "lucide-react";
+import Sidebar from "../Components/Sidebar";
+import { useUser, SignIn } from "@clerk/clerk-react";
 
 const Layout = () => {
-  return (
-    <div>
-      <h1>Layout</h1>
-      <Outlet />
+  const [sidebar, setSetsidebar] = useState(false);
+  const navigate = useNavigate();
+  const { user } = useUser();
+
+  return user ? (
+    <div className="flex flex-col justify-start items-start h-screen">
+      <nav className="w-full min-h-14 px-8 flex items-center justify-between border-b border-gray-200">
+        <img
+          src={assets.logo}
+          alt="logo"
+          onClick={() => navigate("/")}
+          className="cursor-pointer w-32 sm:w-44"
+        />
+        {sidebar ? (
+          <X
+            onClick={() => setSetsidebar((prev) => !prev)}
+            className="w-6 h-6 text-gray-600 sm:hidden"
+          />
+        ) : (
+          <Menu
+            onClick={() => setSetsidebar((prev) => !prev)}
+            className="w-6 h-6 text-gray-600 sm:hidden"
+          />
+        )}
+      </nav>
+      <div className="flex flex-1 h-[calc(100vh-64px)] w-full">
+        <Sidebar sidebar={sidebar} setSidebar={setSetsidebar} />
+        <div className="flex-1 bg-[#F4F7Fb]">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="flex items-center justify-center h-screen">
+      <SignIn />
     </div>
   );
 };
