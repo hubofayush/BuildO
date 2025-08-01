490f0c733539b5cf2bc2f78e3330c04564bb1295
hubofayush <ayushempire03@gmail.com>
2025-08-02 01:56:35 +0530

    feat: add dashboard creation display and item component
    - Added `react-markdown` dependency for rendering markdown content.
    - Implemented `CreationItem` component to display individual creations with expandable details.
    - Updated `Dashboard` to fetch and display total creations and recent creations.
    - Styled dashboard and creation items for better UI/UX.
    - Added CSS reset for Tailwind styles.

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -1,9 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { dummyCreationData } from "../assets/assets";
+import { DollarSign, Gem, Sparkles, SparklesIcon } from "lucide-react";
+import { Protect } from "@clerk/clerk-react";
+import CreationItem from "../Components/CreationItem";
 
 const Dashboard = () => {
+  const [creations, setCreations] = useState([]);
+
+  const getCreations = () => {
+    setCreations(dummyCreationData);
+  };
+
+  useEffect(() => {
+    getCreations();
+  }, []);
+
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className=" overflow-y-auto p-6 h-full">
+      <div className="flex flex-start gap-4 flex-wrap">
+        {/* total creations card */}
+        <div className="flex justify-between rounded-lg item-center w-72 p-4 px-6 bg-white border-gray-200 shadow-lg">
+          <div className="text-slate-600">
+            <p className="text-sm">Total Creation</p>
+            <h1 className="text-xl font-semibold">{creations.length}</h1>
+          </div>
+          <div className="flex  justify-center items-center bg-gradient-to-br from-[#3588F2] to-[#0B0BB7] rounded-lg w-10 h-10 text-white">
+            <Sparkles className="w-5 text-white" />
+          </div>
+        </div>
+        {/* plan card */}
+        <div className="flex flex-wrap items-center justify-between p-4 px-6 w-72 bg-white border border-gray-200 shadow-lg rounded-lg ">
+          <div className="text-gray-600">
+            <p className="text-sm">Active Plan</p>
+            <h2 className="text-xl font-semibold">
+              <Protect plan="premium" fallback="Free">
+                Premium
+              </Protect>
+            </h2>
+          </div>
+          <div className="flex rounded-lg w-10 h-10 bg-gradient-to-br from-[#ff6175] to-[#9e53ee]  text-white justify-center items-center">
+            <Gem className="text-white w-5" />
+          </div>
+        </div>
+      </div>
+
+      <div className="space-y-3">
+        <p className="mt-6 mb-4">Recent Creations</p>
+        {creations.map((item) => (
+          <CreationItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
