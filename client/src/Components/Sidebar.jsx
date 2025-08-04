a896ff95e5b4d092e85679ec8016a67ecf631cae
hubofayush <ayushempire03@gmail.com>
2025-08-04 12:20:05 +0530

    feat: enhance Sidebar and Layout components with improved className binding and z-index for better UI responsiveness

--- client/src/Components/Sidebar.jsx
+++ client/src/Components/Sidebar.jsx
@@ -36,7 +36,7 @@ const Sidebar = ({ sidebar, setSidebar }) => {
   const { signOut, openUserProfile } = useClerk();
   return (
     <div
-      className={`w-60 bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute top-0   bottom-0 ${
+      className={`w-60 bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute top-0   bottom-0  ${
         sidebar ? `translate-x-0` : `max-sm:translate-x-full`
       } transition-all duration-300 ease-in-out`}
     >
