5296be559a82836aced75881ea1303739d4ebf51
hubofayush <ayushempire03@gmail.com>
2025-08-05 10:29:24 +0530

    feat: add ApiError class for standardized error handling

---
+++ server/src/utils/ApiError.js
@@ -0,0 +1,22 @@
+class ApiError extends Error {
+  constructor(
+    message = "something went wrong",
+    statusCode,
+    stack = "",
+    errors = []
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.errors = errors;
+    this.data = null;
+    this.success = false;
+    this.message = message;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.stack);
+    }
+  }
+}
+export { ApiError };
