2faaf5a3cdf6a4b93822df27c462a53da4785ddd
hubofayush <ayushempire03@gmail.com>
2025-08-07 14:35:52 +0530

    connected with backend API

--- client/src/Pages/Community.jsx
+++ client/src/Pages/Community.jsx
@@ -1,56 +1,114 @@
 import React, { useEffect, useState } from "react";
-import { dummyPublishedCreationData } from "../assets/assets";
 import { Heart } from "lucide-react";
 import { useUser } from "@clerk/clerk-react";
+import { useAuth } from "@clerk/clerk-react";
+import toast from "react-hot-toast";
+import axios from "axios";
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 const Community = () => {
-  const [creations, setCreations] = useState([]);
+    const [creations, setCreations] = useState([]);
 
-  const { user } = useUser();
+    const [loading, setLoading] = useState(true);
 
-  const fetchCreations = () => {
-    setCreations(dummyPublishedCreationData);
-  };
-  useEffect(() => {
-    fetchCreations();
-  }, []);
+    const { user } = useUser();
 
-  return (
-    <div className="flex-1 flex h-full flex-col gap-4 p-6">
-      Creations
-      <div className="w-full bg-white h-full rounded-xl overflow-y-auto">
-        {creations.map((creation, index) => (
-          <div
-            className="relative inline-block group pl-3 pt-3 w-full sm:max-w-1/2 lg:max-w-1/3 "
-            key={index}
-          >
-            <img
-              src={creation.content}
-              alt=""
-              className="w-full rounded-lg  object-cover h-full"
-            />
-            <div className="absolute bottom-0 top-0 right-0 left-3 flex gap-2 items-end justify-end group-hover:justify-between p-3 group-hover:bg-gradient-to-b from-transparent to-black/80 text-white rounded-lg">
-              <p className="text-sm hidden group-hover:block">
-                {creation.prompt}
-              </p>
-              <div className="flex gap-1 items-center">
-                <p>{creation.likes.length}</p>
-                <Heart
-                  className={`min-w-5 h-5 hover:scale-110 cursor-pointer
+    const { getToken } = useAuth();
+    const fetchCreations = async () => {
+        try {
+            const { data } = await axios.get("/user/get-publish-creations", {
+                headers: {
+                    Authorization: `Bearer ${await getToken()}`,
+                },
+            });
+
+            if (data.success) {
+                setCreations(data.data);
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            const errorMessage =
+                error.response?.data?.message ||
+                error.message ||
+                "An error occurred";
+            toast.error(errorMessage);
+        }
+        setLoading(false);
+    };
+
+    const toggleLIkeCreation = async (id) => {
+        try {
+            const { data } = await axios.post(
+                "/user/toggle-like-creation",
+                { id },
+                { headers: { Authorization: `Bearer ${await getToken()}` } }
+            );
+
+            if (data.success) {
+                toast.success(data.message);
+                await fetchCreations();
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            const errorMessage =
+                error.response?.data?.message ||
+                error.message ||
+                "An error occurred";
+            toast.error(errorMessage);
+        }
+    };
+    useEffect(() => {
+        if (user) {
+            fetchCreations();
+        }
+    }, [user]);
+
+    return !loading ? (
+        <div className="flex-1 flex h-full flex-col gap-4 p-6">
+            Creations
+            <div className="w-full bg-white h-full rounded-xl overflow-y-auto">
+                {creations.map((creation, index) => (
+                    <div
+                        className="relative inline-block group pl-3 pt-3 w-full sm:max-w-1/2 lg:max-w-1/3 "
+                        key={index}
+                    >
+                        <img
+                            src={creation.content}
+                            alt=""
+                            className="w-full rounded-lg  object-cover h-full"
+                        />
+                        <div className="absolute bottom-0 top-0 right-0 left-3 flex gap-2 items-end justify-end group-hover:justify-between p-3 group-hover:bg-gradient-to-b from-transparent to-black/80 text-white rounded-lg">
+                            <p className="text-sm hidden group-hover:block">
+                                {creation.prompt}
+                            </p>
+                            <div className="flex gap-1 items-center">
+                                <p>{creation.likes.length}</p>
+                                <Heart
+                                    onClick={() =>
+                                        toggleLIkeCreation(creation.id)
+                                    }
+                                    className={`min-w-5 h-5 hover:scale-110 cursor-pointer
                     ${
-                      creation.likes.includes(user.id)
-                        ? "fill-red-500 text-red-600"
-                        : "text-white"
+                        creation.likes.includes(user.id)
+                            ? "fill-red-500 text-red-600"
+                            : "text-white"
                     }
                     `}
-                />
-              </div>
+                                />
+                            </div>
+                        </div>
+                    </div>
+                ))}
             </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+        </div>
+    ) : (
+        <div className="flex items-center justify-center h-full">
+            <span className="w-10 h-10 my-1 border-primary rounded-full border-t-transparent animate-spin border-3"></span>
+        </div>
+    );
 };
 
 export default Community;
--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -1,57 +1,90 @@
 import React, { useEffect, useState } from "react";
-import { dummyCreationData } from "../assets/assets";
-import { DollarSign, Gem, Sparkles, SparklesIcon } from "lucide-react";
-import { Protect } from "@clerk/clerk-react";
+import { Gem, Sparkles } from "lucide-react";
+import { Protect, useAuth } from "@clerk/clerk-react";
 import CreationItem from "../Components/CreationItem";
+import toast from "react-hot-toast";
+import axios from "axios";
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 const Dashboard = () => {
-  const [creations, setCreations] = useState([]);
+    const [creations, setCreations] = useState([]);
 
-  const getCreations = () => {
-    setCreations(dummyCreationData);
-  };
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    getCreations();
-  }, []);
+    const { getToken } = useAuth();
+    const getCreations = async () => {
+        try {
+            const { data } = await axios.get("/user/get-user-creations", {
+                headers: {
+                    Authorization: `Bearer ${await getToken()}`,
+                },
+            });
 
-  return (
-    <div className=" overflow-y-auto p-6 h-full">
-      <div className="flex flex-start gap-4 flex-wrap">
-        {/* total creations card */}
-        <div className="flex justify-between rounded-lg item-center w-72 p-4 px-6 bg-white border-gray-200 shadow-lg">
-          <div className="text-slate-600">
-            <p className="text-sm">Total Creation</p>
-            <h1 className="text-xl font-semibold">{creations.length}</h1>
-          </div>
-          <div className="flex  justify-center items-center bg-gradient-to-br from-[#3588F2] to-[#0B0BB7] rounded-lg w-10 h-10 text-white">
-            <Sparkles className="w-5 text-white" />
-          </div>
+            if (data.success) {
+                setCreations(data.data);
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            const errorMessage =
+                error.response?.data?.message ||
+                error.message ||
+                "An error occurred";
+            toast.error(errorMessage);
+        }
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        getCreations();
+    }, []);
+
+    return (
+        <div className=" overflow-y-auto p-6 h-full">
+            <div className="flex flex-start gap-4 flex-wrap">
+                {/* total creations card */}
+                <div className="flex justify-between rounded-lg item-center w-72 p-4 px-6 bg-white border-gray-200 shadow-lg">
+                    <div className="text-slate-600">
+                        <p className="text-sm">Total Creation</p>
+                        <h1 className="text-xl font-semibold">
+                            {creations.length}
+                        </h1>
+                    </div>
+                    <div className="flex  justify-center items-center bg-gradient-to-br from-[#3588F2] to-[#0B0BB7] rounded-lg w-10 h-10 text-white">
+                        <Sparkles className="w-5 text-white" />
+                    </div>
+                </div>
+                {/* plan card */}
+                <div className="flex flex-wrap items-center justify-between p-4 px-6 w-72 bg-white border border-gray-200 shadow-lg rounded-lg ">
+                    <div className="text-gray-600">
+                        <p className="text-sm">Active Plan</p>
+                        <h2 className="text-xl font-semibold">
+                            <Protect plan="premium" fallback="Free">
+                                Premium
+                            </Protect>
+                        </h2>
+                    </div>
+                    <div className="flex rounded-lg w-10 h-10 bg-gradient-to-br from-[#ff6175] to-[#9e53ee]  text-white justify-center items-center">
+                        <Gem className="text-white w-5" />
+                    </div>
+                </div>
+            </div>
+
+            {loading ? (
+                <div className="flex justify-center items-center h-3/4">
+                    <div className="h-11 w-11 rounded-full border-purple-500 border-3 border-t-transparent animate-spin"></div>
+                </div>
+            ) : (
+                <div className="space-y-3">
+                    <p className="mt-6 mb-4">Recent Creations</p>
+                    {creations.map((item) => (
+                        <CreationItem key={item.id} item={item} />
+                    ))}
+                </div>
+            )}
         </div>
-        {/* plan card */}
-        <div className="flex flex-wrap items-center justify-between p-4 px-6 w-72 bg-white border border-gray-200 shadow-lg rounded-lg ">
-          <div className="text-gray-600">
-            <p className="text-sm">Active Plan</p>
-            <h2 className="text-xl font-semibold">
-              <Protect plan="premium" fallback="Free">
-                Premium
-              </Protect>
-            </h2>
-          </div>
-          <div className="flex rounded-lg w-10 h-10 bg-gradient-to-br from-[#ff6175] to-[#9e53ee]  text-white justify-center items-center">
-            <Gem className="text-white w-5" />
-          </div>
-        </div>
-      </div>
-
-      <div className="space-y-3">
-        <p className="mt-6 mb-4">Recent Creations</p>
-        {creations.map((item) => (
-          <CreationItem key={item.id} item={item} />
-        ))}
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Dashboard;
