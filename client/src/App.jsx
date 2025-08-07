41fe5583782c632f1454a75e4b89d55601472a97
hubofayush <ayushempire03@gmail.com>
2025-08-07 12:07:56 +0530

    Connected Write article with backend api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,35 +9,43 @@ import GenerateImages from "./Pages/GenerateImages";
 import ReamoveObject from "./Pages/ReamoveObject";
 import RemoveBackgorund from "./Pages/RemoveBackgorund";
 import ReviewResume from "./Pages/ReviewResume";
-import { useAuth } from "@clerk/clerk-react";
-import { useEffect } from "react";
+// import { useAuth } from "@clerk/clerk-react";
+// import { useEffect } from "react";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
-  const { getToken } = useAuth();
+    // const { getToken } = useAuth();
 
-  useEffect(() => {
-    getToken().then((token) => console.log(token));
-  }, []);
+    // useEffect(() => {
+    //     getToken().then((token) => console.log(token));
+    // }, []);
 
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        // nested routes //
-        <Route path="/ai" element={<Layout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="blog-titles" element={<BlogTitles />} />
-          <Route path="community" element={<Community />} />
-          <Route path="generate-images" element={<GenerateImages />} />
-          <Route path="remove-object" element={<ReamoveObject />} />
-          <Route path="remove-background" element={<RemoveBackgorund />} />
-          <Route path="review-resume" element={<ReviewResume />} />
-          <Route path="write-article" element={<WriteArticle />} />
-        </Route>
-        // end of nested routes //
-      </Routes>
-    </>
-  );
+    return (
+        <>
+            <Toaster />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                // nested routes //
+                <Route path="/ai" element={<Layout />}>
+                    <Route index element={<Dashboard />} />
+                    <Route path="blog-titles" element={<BlogTitles />} />
+                    <Route path="community" element={<Community />} />
+                    <Route
+                        path="generate-images"
+                        element={<GenerateImages />}
+                    />
+                    <Route path="remove-object" element={<ReamoveObject />} />
+                    <Route
+                        path="remove-background"
+                        element={<RemoveBackgorund />}
+                    />
+                    <Route path="review-resume" element={<ReviewResume />} />
+                    <Route path="write-article" element={<WriteArticle />} />
+                </Route>
+                // end of nested routes //
+            </Routes>
+        </>
+    );
 };
 
 export default App;
